97cfa21193054307af8daa7ce9acf14f309d089c
mostafaashraf204 <mostafaashraf3100@gmaill.com>
2023-03-13 20:20:46 +0200

    first nav and home page by angular

---
+++ src/app/nav-bar/nav-bar.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-nav-bar',
+  templateUrl: './nav-bar.component.html',
+  styleUrls: ['./nav-bar.component.css']
+})
+export class NavBarComponent {
+images="../../assets/images/logo.png.webp"
+wallpaper="../../assets/images/bg.jpg.webp"
+}
